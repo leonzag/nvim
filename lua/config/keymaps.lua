6cae16cf281b8ea550b96cec3cb288beefa6ab87
Leonid Zagumennov <leonzag997@gmail.com>
2025-03-23 02:02:51 +0300

    - rm unnecessary + format

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,7 +27,6 @@ map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 
--- map({"i", "n", "x"}, "<A-e>", rhs, opts)
 map({ "n", "v" }, "<S-l>", "$hl")
 map({ "n", "v" }, "<S-h>", "^")
 map({ "x", "s" }, "<C-w>", "d", { desc = "Delete lines" })
@@ -35,16 +34,15 @@ map({ "x", "s" }, "<C-w>", "d", { desc = "Delete lines" })
 map("n", "<A-.>", "<cmd>BufferLineCycleNext<cr>")
 map("n", "<A-,>", "<cmd>BufferLineCyclePrev<cr>")
 
--- map({ "i", "x", "n", "s" }, "<A-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 map({ "i", "x", "n", "s" }, "<A-w>", "<cmd>w<cr><esc>", { desc = "Save file" })
 map("n", "gl", vim.diagnostic.open_float, { desc = "Line diagnostic" })
 
 map({ "i", "n", "x", "o" }, "<A-/>", "<esc>gcc", { desc = "Toggle comment", remap = true })
-map({ "i", "n", "x", "o" }, "<C-/>", "<esc>gcc", { desc = "Toggle comment", remap = true })
 map("v", "<A-/>", "gc", { desc = "Toggle comment selection", remap = true })
-map("v", "<C-/>", "gc", { desc = "Toggle comment selection", remap = true })
 
-map({ "i", "x", "n", "s" }, "<A-c>", function () Snacks.bufdelete() end, { desc = "Delete Buffer" })
+map({ "i", "x", "n", "s" }, "<A-c>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
 map({ "i", "x", "n", "s" }, "<A-q>", "<cmd>qa<cr>", { desc = "Quit" })
 
 -- Clear search with <esc>
