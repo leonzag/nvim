398f03c1847bc57571b3760a9c39dd801352273d
Leonid Zagumennov <leonzag997@gmail.com>
2025-03-23 02:04:47 +0300

    + set correct moves out from terminal window

--- lua/plugins/editor/terminal.lua
+++ lua/plugins/editor/terminal.lua
@@ -3,11 +3,12 @@ return {
   init = function()
     function _G.set_terminal_keymaps()
       local opts = { buffer = 0 }
+      vim.keymap.set("t", "<esc><esc>", [[<C-\><C-n>]], opts)
+      vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
       vim.keymap.set("t", "<C-w>", [[<C-\><C-w>]], opts)
-      vim.keymap.set("t", "<C-l>", [[<C-\><C-l>]], opts)
-      -- vim.keymap.set("t", "<C-k>", [[<C-\><C-k>]], opts)
-      vim.keymap.set("t", "<C-j>", [[<C-\><C-j>]], opts)
-      -- vim.keymap.set("t", "<C-h>", [[<C-\><C-h>]], opts)
     end
 
     -- if you only want these mappings for toggle term use term://*toggleterm#* instead
