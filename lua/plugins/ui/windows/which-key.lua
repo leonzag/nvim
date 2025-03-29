961e46319153afa4f2cda0c87622588ff2df7248
Leonid Zagumennov <leonzag997@gmail.com>
2025-03-29 16:47:32 +0300

    - which-key: disable default bufferline keys expanding (switch bufs keys)

--- lua/plugins/ui/windows/which-key.lua
+++ lua/plugins/ui/windows/which-key.lua
@@ -4,9 +4,21 @@ return {
   opts = {
     preset = "modern",
     win = {
-      -- border = { "", "⎽", "", "", "", "", "", "" },
       wo = {
         winblend = 10,
+      },
+    },
+    spec = {
+      {
+        {
+          "<leader>b",
+          group = "buffer",
+          expand = function()
+            -- disable expand, use Alt+[1-0] for switch buffers instead
+            -- return require("which-key.extras").expand.buf()
+            return {}
+          end,
+        },
       },
     },
   },
