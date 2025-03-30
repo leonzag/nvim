1f13a5c5589cbbec8f5199c92bca6773fcb845ba
Leonid Zagumennov <leonzag997@gmail.com>
2025-03-30 15:47:34 +0300

    + snacks indent+scope change char to 'left v-line'

---
+++ lua/plugins/extras/ui/snacks_indent.lua
@@ -0,0 +1,16 @@
+return {
+  "snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    indent = {
+      indent = {
+        char = "▏",
+        -- char = "│", -- default
+      },
+      scope = {
+        char = "▏",
+        -- char = "│", -- default
+      },
+    },
+  },
+}
