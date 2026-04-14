7b9af42eadc08ef841252ba1a55f3687ec0e50f9
Leonid Zagumennov <leonzag997@gmail.com>
2026-04-14 19:55:30 +0300

    ~ lint html-django with djlint

--- lua/plugins/linting/lint.lua
+++ lua/plugins/linting/lint.lua
@@ -5,23 +5,7 @@ return {
       linters_by_ft = {
         fish = { "fish" },
         zsh = { "zsh" }, -- not working for unknown reason
-        -- -- Use the "*" filetype to run linters on all filetypes.
-        -- ["*"] = { "global linter" },
-        -- -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
-        -- ["_"] = { "fallback linter" },
-        -- -- LazyVim extension to easily override linter options
-        -- -- or add custom linters.
-        -- ---@type table<string,table>
-        -- linters = {
-        --   -- Example of using selene only when a selene.toml file is present
-        --   selene = {
-        --     -- `condition` is another LazyVim extension that allows you to
-        --     -- dynamically enable/disable linters based on the context.
-        --     condition = function(ctx)
-        --       return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
-        --     end,
-        --   },
-        -- },
+        htmldjango = { "djlint" },
       },
     },
     config = function()
