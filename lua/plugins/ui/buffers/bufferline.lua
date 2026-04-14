22503b91e6d96682f207f0bf37aab2090b448a51
Leonid Zagumennov <leonzag997@gmail.com>
2026-04-14 20:03:13 +0300

    upd buffers+scrollbar

--- lua/plugins/ui/buffers/bufferline.lua
+++ lua/plugins/ui/buffers/bufferline.lua
@@ -4,12 +4,13 @@ return {
   opts = {
     options = {
       indicator = {
-        icon = "▍", -- this should be omitted if indicator style is not 'icon'
-        style = "icon",
+        -- icon = "▐", -- this should be omitted if indicator style is not 'icon'
+        style = "none",
       },
       numbers = function(opts)
         return string.format("%s", opts.raise(opts.ordinal))
       end,
+      persist_buffer_sort = true,
       buffer_close_icon = "✖",
       sort_by = "directory",
       offsets = {
@@ -32,16 +33,6 @@ return {
     { "<A-[>", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer left" },
     { "<S-h>", false },
     { "<S-l>", false },
-    { "<A-1>", "<cmd>BufferLineGoToBuffer 1<cr>", desc = "Go to Buf 1" },
-    { "<A-2>", "<cmd>BufferLineGoToBuffer 2<cr>", desc = "Go to Buf 2" },
-    { "<A-3>", "<cmd>BufferLineGoToBuffer 3<cr>", desc = "Go to Buf 3" },
-    { "<A-4>", "<cmd>BufferLineGoToBuffer 4<cr>", desc = "Go to Buf 4" },
-    { "<A-5>", "<cmd>BufferLineGoToBuffer 5<cr>", desc = "Go to Buf 5" },
-    { "<A-6>", "<cmd>BufferLineGoToBuffer 6<cr>", desc = "Go to Buf 6" },
-    { "<A-7>", "<cmd>BufferLineGoToBuffer 7<cr>", desc = "Go to Buf 7" },
-    { "<A-8>", "<cmd>BufferLineGoToBuffer 8<cr>", desc = "Go to Buf 8" },
-    { "<A-9>", "<cmd>BufferLineGoToBuffer 9<cr>", desc = "Go to Buf 9" },
-    { "<A-0>", "<cmd>BufferLineGoToBuffer 10<cr>", desc = "Go to Buf 10" },
     { "<leader>bs", "", desc = "Sort buffers" },
     { "<leader>bse", "<cmd>BufferLineSortByExtension<cr>", desc = "Sort by ext" },
     { "<leader>bsd", "<cmd>BufferLineSortByDirectory<cr>", desc = "Sort by dir" },
--- lua/plugins/ui/scrollbar/scrollbar.lua
+++ lua/plugins/ui/scrollbar/scrollbar.lua
@@ -2,25 +2,10 @@ return {
   {
     "petertriho/nvim-scrollbar",
     event = "BufReadPost",
-    config = function()
-      local scrollbar = require("scrollbar")
-      -- local colors = require("tokyonight.colors").setup()
-      local colors = require("catppuccin.palettes").get_palette()
-      scrollbar.setup({
-        hide_if_all_visible = true, -- Hides everything if all lines are visible
-        throttle_ms = 50,
-        handle = { color = colors.crust },
-        excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify" },
-        marks = {
-          Cursor = { text = " ", color = colors.base },
-          Search = { color = colors.teal },
-          Error = { color = colors.red },
-          Warn = { color = colors.peach },
-          Info = { color = colors.green },
-          Hint = { color = colors.blue },
-          Misc = { color = colors.pink },
-        },
-      })
-    end,
+    opts = {
+      hide_if_all_visible = true, -- Hides everything if all lines are visible
+      throttle_ms = 50,
+      excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify" },
+    },
   },
 }
