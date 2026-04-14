746ac7dd0f51912718541a1834cb1be3a63db47b
Leonid Zagumennov <leonzag997@gmail.com>
2026-04-14 19:50:19 +0300

    configure trouble.nvim

--- lua/plugins/editor/trouble.lua
+++ lua/plugins/editor/trouble.lua
@@ -1,6 +1,11 @@
 return {
   "folke/trouble.nvim",
+  opts = {
+    warn_no_results = false,
+  },
   keys = {
     { "<A-x>", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+    { "<A-i>", "<cmd>Trouble lsp toggle<cr>", desc = "Lsp (Trouble)" },
+    { "<A-s>", "<cmd>Trouble symbols toggle<cr>", desc = "LspSymbols (Trouble)" },
   },
 }
