a7f061216566609663fb5c05e76dcf2108cad470
Leonid Zagumennov <leonzag997@gmail.com>
2026-04-15 00:36:43 +0300

    - rm breadcrumbs import

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -3,6 +3,5 @@ return {
   { import = "plugins.ui.windows" },
   { import = "plugins.ui.buffers" },
   { import = "plugins.ui.statusline" },
-  { import = "plugins.ui.breadcrumbs" },
   { import = "plugins.ui.scrollbar" },
 }
